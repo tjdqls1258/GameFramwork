9081a4c883e90aac25ba093031f69671f613f09a
학과 게임 <hoseogame@hoseogame.onmicrosoft.com>
2018-11-02 12:07:38 +0900

    11.02 PP12.Vector2D

---
+++ PP12.Vector2D/Game.h
@@ -0,0 +1,52 @@
+#pragma once
+#include "Player.h"
+#include "Enemy.h"
+#include <vector>
+#include "LoaderParams.h"
+
+
+class Game
+{
+public:
+	
+	bool init(const char* title, int xpos, int ypos,
+		int width, int height, bool fullscreen);
+
+	void render();
+	void update();
+	void handleEvents();
+	void clean();
+	bool running() { return m_bRunning; }
+	SDL_Renderer* getRenderer() const { return m_pRenderer; }
+
+	static Game* Instance()
+	{
+		if (s_pInstance == 0)
+		{
+			s_pInstance = new Game();
+			return s_pInstance;
+		}
+		return s_pInstance;
+	}
+
+private:
+	Game();
+	static Game* s_pInstance;
+
+	bool m_bRunning = true;
+
+	//int m_currentFrame;
+
+	SDL_Window * m_pWindow = 0;
+	SDL_Renderer* m_pRenderer = 0;
+	std::vector<GameObject*> m_gameObjects;
+
+	/*GameObject* m_go;
+	GameObject* m_player;
+	GameObject* m_enemy;*/
+	
+};
+
+
+
+typedef Game TheGame;
